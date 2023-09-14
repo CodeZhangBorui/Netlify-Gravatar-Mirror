99062e5bb3573865bb0eb498151ad69ae0bf1a99
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2023-09-14 15:01:12 +0800

    🌅

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Netlify-Gravatar-Mirror
+
 A netlify project to build a Gravatar image
+
+## How to use
+
+Click this button to deploy to your Netlify Account:
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/CodeZhangBorui/Netlify-Gravatar-Mirror)
---
+++ edge_functions/gravatar.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param {Request} request
+ * @param {*} context
+ */
+export default async function (request) {
+    if (request.method === "GET") {
+        try {
+            const url = await request.url;
+            console.log("Request URL: ", url);
+            const res = await fetch("https://gravatar.com/avatar/" + url.split("/avatar/")[1], {
+                method: "GET",
+            });
+            const resp = new Response(
+                res,
+                {
+                    status: 200,
+                }
+            );
+            resp.headers.set("Access-Control-Allow-Origin", "*");
+            resp.headers.set("Access-Control-Allow-Methods", "GET");
+            resp.headers.set("Access-Control-Allow-Headers", "Content-Type");
+            resp.headers.set("Access-Control-Max-Age", `${86400 * 30}`);
+            return resp;
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+    return new Response("Gravatar only supports GET requests!");
+}
